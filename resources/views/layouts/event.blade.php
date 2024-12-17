2d5ef53eab7c178055c4b39b2b76211b97599007
Alexander Zemskov <alexzemskov1999@gmail.com>
2024-12-17 14:41:34 +0300

    Add Authentication

--- resources/views/layouts/event.blade.php
+++ resources/views/layouts/event.blade.php
@@ -12,20 +12,49 @@
 <div>
     <nav class="navbar navbar-expand-sm navbar-light mb-3" id="neubar">
         <div class="navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav me-auto"> <!-- Добавляем me-auto для выравнивания влево -->
                 <li class="nav-item">
-                    <a class="nav-link mx-2 active" aria-current="page" href="{{route('events.main')}}">Main</a>
+                    <a class="nav-link mx-2 active" aria-current="page" href="{{ route('events.main') }}">Main</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mx-2" href="{{route('events.about')}}">About</a>
+                    <a class="nav-link mx-2" href="{{ route('events.about') }}">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mx-2" href="{{route('events.index')}}">Events</a>
+                    <a class="nav-link mx-2" href="{{ route('events.index') }}">Events</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mx-2" href="{{route('events.contacts')}}">Contacts</a>
+                    <a class="nav-link mx-2" href="{{ route('events.contacts') }}">Contacts</a>
                 </li>
             </ul>
+
+            <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                            <div>{{ Auth::user()->name }}</div>
+                            <div class="ms-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <x-dropdown-link :href="route('logout')"
+                                             onclick="event.preventDefault(); this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
+            </div>
         </div>
     </nav>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Events\StoreController;
 use App\Http\Controllers\Events\CreateController;
 use App\Http\Controllers\Events\IndexController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProfileController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -29,3 +29,16 @@ Route::group(['namespace' => 'App\Http\Controllers\Events'], function () {
     Route::get('/about', \App\Http\Controllers\Events\AboutController::class)->name('events.about');
     Route::get('/contacts', \App\Http\Controllers\Events\ContactsController::class)->name('events.contacts');
 });
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
